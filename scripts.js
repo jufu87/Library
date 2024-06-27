2e9ab5443946716ec5c71e12917c04bf41a5a482
jufu87 <168488450+jufu87@users.noreply.github.com>
2024-06-27 19:10:50 +0200

    Create function to add book
    Create a function to a book in a new list inside a new div.

--- scripts.js
+++ scripts.js
@@ -3,31 +3,67 @@
 // let pages = document.querySelector("#pages").value;
 // let release = document.querySelector("#release").value;
 // let read = document.querySelector("#read").value;
-
+let newBookBtn = document.querySelector("#newbook-button");
+document.querySelector("#form").style.visibility = "hidden";
+let right = document.querySelector(".right");
 
 const myLibrary = [];
 
-function Book(title, author, pages, release, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.release = release;
     this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, release, read) {
-    const newBook = new Book(title, author, pages, release, read);
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-addBookToLibrary("Night Watch", "Terry Pratchett", 464, 2002, true);
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, 1937, true);
-// addBookToLibrary(title, author, pages, release, read);
+addBookToLibrary("Night Watch", "Terry Pratchett", 464, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+// addBookToLibrary(title, author, pages, read);
 
-function loopThroughLibrary(library){
-    for (let i = 0; i < library.length; i++) {
-        console.log(library[i]);
-    }
+function createCard(library) {
+    library.forEach(function (book) {
+        let card = document.createElement("div");
+        let cardUl = document.createElement("ul")
+        let cardLiTitle = document.createElement("li")
+        let cardLiAuthor = document.createElement("li")
+        let cardLiPages = document.createElement("li")
+        let cardLiRead = document.createElement("li")
+
+        card.classList.add("card");
+
+        right.appendChild(card);
+        card.appendChild(cardUl);
+        cardUl.appendChild(cardLiTitle);
+        cardUl.appendChild(cardLiAuthor);
+        cardUl.appendChild(cardLiPages);
+        cardUl.appendChild(cardLiRead);
+
+        cardLiTitle.textContent = book.title;
+        cardLiAuthor.textContent = book.author;
+        cardLiPages.textContent = book.pages;
+        cardLiRead.textContent = book.read;
+        // console.log(book.title);
+        // console.log(book.author);
+        // console.log(book.pages);
+        // console.log(book.read);
+    })
 }
 
-loopThroughLibrary(myLibrary);
+createCard(myLibrary);
+
+
+
+
+
+
+// eventlistener below is to make form visible
+
+newBookBtn.addEventListener('click', function () {
+    document.querySelector("#form").style.visibility = "visible";
+    console.log("clicked");
+});
